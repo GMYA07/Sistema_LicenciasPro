d8d5b127429ca0b1777b851c996b87d8b35a57a0
Yael Matamoros <89718678+GMYA07@users.noreply.github.com>
2026-05-19 16:42:58 -0600

    Trabajando la conexion de la bdd

--- core/Database.php
+++ core/Database.php
@@ -1,1 +1,14 @@
 <?php
+class Database {
+    private $conexion;
+
+    public function __construct(){
+        //cargamos las variables de entorno
+        $host = $_ENV['DB_HOST'];
+        $user = $_ENV['DB_USER'];
+        $pass = $_ENV['DB_PASS'] ?? '';
+        $name = $_ENV['DB_NAME'];
+
+        $dsn = "mysql:host={$host}";
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
+require_once __DIR__ . '/../vendor/autoload.php';
+//Configuracion que se usa para poder usar las variables .env
+// 2. Inicializar y cargar el archivo .env desde la raíz del proyecto
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
 require_once '../config/app.php';
 require_once '../core/Router.php';
 
